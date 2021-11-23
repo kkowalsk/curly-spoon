175755ca0c5a6e57e3476b598d526926e5cfd584
kevin kowalski <kevin.d.kowalski@gmail.com>
2021-11-23 15:06:12 -0700

    add usage, check for help, and use a global status variable

--- main.py
+++ main.py
@@ -2,31 +2,44 @@
 # todo
 #
 
+import os
 import sys
 import time
+import typing
 import traceback
-from typing import Annotated, TypeVar
+
 from goprocam import GoProCamera, constants
 
 # constants
 AP_MAC_KEY = 'ap_mac'
-AP_MAC_VALUE = None
 
 # globals
 DEBUG = True
-GOPRO_CAM = Annotated[GoProCamera.GoPro, 'global GoPro camera handle']  #TypeVar('GOPRO_CAM', GoProCamera.GoPro, None)
+IS_CAMERA_CONNECTED = False
+AP_MAC_VALUE = None
+GOPRO_CAM = typing.Annotated[GoProCamera.GoPro, 'global GoPro camera handle']
 
-def init_camera():
-    global GOPRO_CAM
+def show_usage():
+    print("usage:\n{} [-h || --help]".format(os.path.basename(__file__)))
+
+def open_camera_connection():
+    global GOPRO_CAM, IS_CAMERA_CONNECTED, AP_MAC_VALUE
+
     GOPRO_CAM = GoProCamera.GoPro(mac_address='AA:BB:CC:DD:EE:FF') # the default mac address is fine but for documentation purposes init with
     GOPRO_CAM.power_on()
+    IS_CAMERA_CONNECTED = True
     print("...")
     info = GOPRO_CAM.infoCamera()
     AP_MAC_VALUE = info[AP_MAC_KEY]
     print("...")
 
+def close_camera_connection():
+    global IS_CAMERA_CONNECTED
+    GOPRO_CAM.power_off()
+    IS_CAMERA_CONNECTED = False
+
 def is_camera_ready():
-    return GOPRO_CAM != None
+    return IS_CAMERA_CONNECTED
 
 def start_video_capture():
     GOPRO_CAM.shoot_video()
@@ -45,24 +58,32 @@ def download_last_video():
     GOPRO_CAM.downloadLastMedia(custom_filename="download_test.mp4") # will override existing filenames
 
 def video_test():
-    init_camera()
+    open_camera_connection()
     if is_camera_ready():
         start_video_capture()
         time.sleep(5)
         end_video_capture()
         download_last_video()
 
-def main():
-    try:
-        video_test()
-    except KeyboardInterrupt:
-        print("Closing")
-    except:
-        print(traceback.format_exc())
-    finally:
-        if is_camera_ready:
-            GOPRO_CAM.power_off()
-        sys.exit()
+def main(argv):
+    if len(argv) == 2 and ( argv[1] == '-h' or argv == '--help' ):
+        show_usage()
+    else:
+        try:
+            video_test()
+        except KeyboardInterrupt:
+            print("Closing")
+        except:
+            print(traceback.format_exc())
+        finally:
+            pass
+
+    program_exit()
+
+def program_exit():
+    if is_camera_ready():
+        close_camera_connection()
+    sys.exit()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
