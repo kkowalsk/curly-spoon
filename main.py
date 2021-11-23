0e696715eafc90ac7acfae8a128f5e6094a7cf3b
kevin kowalski <kevin.d.kowalski@gmail.com>
2021-11-23 14:39:39 -0700

    test logic flow to init, capture, end, and download video

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import sys
 import time
+import traceback
+from typing import Annotated, TypeVar
 from goprocam import GoProCamera, constants
 
 # constants
@@ -11,7 +13,8 @@ AP_MAC_KEY = 'ap_mac'
 AP_MAC_VALUE = None
 
 # globals
-GOPRO_CAM = None
+DEBUG = True
+GOPRO_CAM = Annotated[GoProCamera.GoPro, 'global GoPro camera handle']  #TypeVar('GOPRO_CAM', GoProCamera.GoPro, None)
 
 def init_camera():
     global GOPRO_CAM
@@ -22,16 +25,43 @@ def init_camera():
     AP_MAC_VALUE = info[AP_MAC_KEY]
     print("...")
 
+def is_camera_ready():
+    return GOPRO_CAM != None
+
+def start_video_capture():
+    GOPRO_CAM.shoot_video()
+    if DEBUG:
+        print("is recording {}".format(GOPRO_CAM.IsRecording()))
+        sys.stdout.flush()
+
+def end_video_capture():
+    GOPRO_CAM.shutter(constants.stop)
+    time.sleep(3)
+    if DEBUG:
+        print("is recording {}".format(GOPRO_CAM.IsRecording()))
+        sys.stdout.flush()
+
+def download_last_video():
+    GOPRO_CAM.downloadLastMedia(custom_filename="download_test.mp4") # will override existing filenames
+
+def video_test():
+    init_camera()
+    if is_camera_ready():
+        start_video_capture()
+        time.sleep(5)
+        end_video_capture()
+        download_last_video()
 
 def main():
     try:
-        init_camera()
+        video_test()
     except KeyboardInterrupt:
         print("Closing")
     except:
-        pass
+        print(traceback.format_exc())
     finally:
-        GOPRO_CAM.power_off()
+        if is_camera_ready:
+            GOPRO_CAM.power_off()
         sys.exit()
 
 if __name__ == "__main__":
