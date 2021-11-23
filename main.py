c6d871e1c7691df5ebcb4c72439416c19dfa2af6
kevin kowalski <kevin.d.kowalski@gmail.com>
2021-11-23 14:03:36 -0700

    add main function and always turn off camera

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
+#
+# todo
+#
 
+import sys
+import time
+from goprocam import GoProCamera, constants
+
+# constants
 AP_MAC_KEY = 'ap_mac'
 AP_MAC_VALUE = None
 
-from goprocam import GoProCamera, constants
-import time
-gpCam = GoProCamera.GoPro(mac_address='AA:BB:CC:DD:EE:FF') # the default mac address is fine but for documentation purposes init with
-gpCam.power_on()
-print("...")
-info = gpCam.infoCamera()
-AP_MAC_VALUE = info[AP_MAC_KEY]
-print("...")
-gpCam.power_off()
+# globals
+GOPRO_CAM = None
+
+def init_camera():
+    global GOPRO_CAM
+    GOPRO_CAM = GoProCamera.GoPro(mac_address='AA:BB:CC:DD:EE:FF') # the default mac address is fine but for documentation purposes init with
+    GOPRO_CAM.power_on()
+    print("...")
+    info = GOPRO_CAM.infoCamera()
+    AP_MAC_VALUE = info[AP_MAC_KEY]
+    print("...")
+
+
+def main():
+    try:
+        init_camera()
+    except KeyboardInterrupt:
+        print("Closing")
+    except:
+        pass
+    finally:
+        GOPRO_CAM.power_off()
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
