68b9d5f0b7448757f1c51bd437da5706df3f9a64
kevin kowalski <kevin.d.kowalski@gmail.com>
2021-11-24 12:48:34 -0700

    connect to wifi function

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import sys
 import time
 import typing
 import traceback
+import subprocess
 
 from goprocam import GoProCamera, constants
 
 # constants
 AP_MAC_KEY = 'ap_mac'
+GOPRO_WIFI_SSID = 'go_dg_pro'
 
 # globals
 DEBUG = True
@@ -21,6 +23,23 @@ GOPRO_CAM = typing.Annotated[GoProCamera.GoPro, 'global GoPro camera handle']
 
 def show_usage():
     print("usage:\n{} [-h || --help]".format(os.path.basename(__file__)))
+
+def is_connected_gopro_wifi():
+    result = subprocess.check_output(['netsh', 'wlan', 'show', 'interfaces'])
+    return GOPRO_WIFI_SSID in str(result)
+
+def connect_gopro_wifi():
+    if not is_connected_gopro_wifi():
+        if DEBUG:
+            print("connecting to SSID {}".format(GOPRO_WIFI_SSID))
+        result = os.system('cmd /c \"netsh wlan connect name={}\"'.format(GOPRO_WIFI_SSID))
+        if DEBUG:
+            print("netsh result: {}".format(result))
+            sys.stdout.flush()
+        time.sleep(3)
+    else:
+        if DEBUG:
+            print("already connected to gopro wifi")
 
 def open_camera_connection():
     global GOPRO_CAM, IS_CAMERA_CONNECTED, AP_MAC_VALUE
@@ -58,6 +77,7 @@ def download_last_video():
     GOPRO_CAM.downloadLastMedia(custom_filename="download_test.mp4") # will override existing filenames
 
 def video_test():
+    connect_gopro_wifi()
     open_camera_connection()
     if is_camera_ready():
         start_video_capture()
